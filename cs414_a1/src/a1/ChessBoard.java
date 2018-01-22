a7864bd789e092feb49461b1313a999311f7d740
artracy <atracy@colostate.edu>
2018-01-22 15:35:48 -0500

    Created new assignment 1 java project.

---
+++ cs414_a1/src/a1/ChessBoard.java
@@ -0,0 +1,17 @@
+package a1;
+
+public class ChessBoard {
+	
+	private ChessPiece[][] board;
+	
+	public void initialize() {
+		
+		
+	}
+	
+	public ChessPiece getPeice(String position) throws IllegalPositionException {
+		return null;
+	}
+	
+
+}
---
+++ cs414_a1/src/a1/ChessPiece.java
@@ -0,0 +1,5 @@
+package a1;
+
+public abstract class ChessPiece {
+
+}

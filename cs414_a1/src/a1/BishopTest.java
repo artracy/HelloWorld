2d9bb5c150d0248f9740c8cd460ecb020f4c131c
artracy <atracy@colostate.edu>
2018-01-22 23:06:38 -0500

    Created the classes for the JUnit test cases and test suite.

---
+++ cs414_a1/src/a1/BishopTest.java
@@ -0,0 +1,14 @@
+package a1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BishopTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
